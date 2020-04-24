5eeb36c412fce4da50c5f1a49a5484bc73211bfc
Mateusz Pietrowiak <matas8@protonmail.com>
2020-04-24 19:41:52 +0200

    Simple UI mockup

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,30 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
-
 import './App.css';
 
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    
-    return body;
-  };
-  
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    
-    this.setState({ responseToPost: body });
-  };
-  
-render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-      </div>
-    );
-  }
-}
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import Paper from '@material-ui/core/Paper';
+import { styled } from '@material-ui/core/styles';
+
+const StyledPaper = styled(Paper)({
+  padding: '20px'
+});
+
+const StyledButton = styled(Button)({
+  marginRight: '10px'
+});
+
+const App = () => {
+  return (
+    <Container>
+      <StyledPaper>
+        <h1>Push notifications demo</h1>
+        <StyledButton variant="contained" color="primary">Subscribe to push</StyledButton>
+        <StyledButton variant="contained" color="secondary">Unsubscribe from push</StyledButton>
+      </StyledPaper>
+    </Container>
+  )
+};
 
 export default App;
