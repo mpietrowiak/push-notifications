e9c1396ec3ca05b2f1a91f53c87966d07acd1eec
Mateusz Pietrowiak <matas8@protonmail.com>
2020-04-25 19:53:05 +0200

    Add simple notifications sending logic

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,57 @@ const StyledButton = styled(Button)({
   marginBottom: '10px'
 });
 
+
+const publicVapidKey = 'BHf1bzlOSSbU8lI5zmqAkOY4nBHG9kdQAB6oy1aYWk-kZ_xtpn2EjQcnhVx71ItDiWeXlW3bOLuDy9QWqjiEdJA';
+
+if ('serviceWorker' in navigator) {
+  send().catch(err => console.error(err));
+}
+
+// register the SW, register the push, send notification
+async function send() {
+  console.log('Registering service worker...');
+  const register = await navigator.serviceWorker.register('/worker.js', {
+    scope: '/'
+  });
+  console.log('Service worker Registered...');
+
+  // Register push
+  console.log('Registering push...');
+  const subscription = await register.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+  });
+  console.log('Push registered');
+
+  // Send push notification
+  console.log('Sending push....');
+  await fetch('/subscribe', {
+    method: 'POST',
+    body: JSON.stringify(subscription),
+    headers: {
+      'content-type': 'application/json'
+    }
+  });
+  console.log('Push sent...');
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/');
+ 
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+ 
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+ 
+
 const App = () => {
   return (
     <Container>
--- server.js
+++ server.js
@@ -1,22 +1,36 @@
+require('dotenv').config();
+
+if (!process.env.VAPID_PUBLIC_KEY || !process.env.VAPID_PRIVATE_KEY) {
+  console.log('Please specify VAPID_PUBLIC_KEY and VAPID_PRIVATE_KEY variables!');
+  return;
+}
+
+const webpush = require('web-push');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+
 const port = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+webpush.setVapidDetails(
+  'mailto:test@test.com',
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+);
+
+app.post('/subscribe', (req, res) => {
+  const subscription = req.body;
+  res.status(201).json({});
+  const payload = JSON.stringify({ title: 'Push test '});
+  webPush.sendNotification(subscription, payload).catch(err => console.error(err));
 });
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
